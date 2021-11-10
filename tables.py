87c70e7f6cf883076a51126fd12bad47831d9cbf
fahid latheef <fahidlatheef@fahid-latheefs-MacBook-Pro.local>
2021-11-11 02:18:13 +0530

    Refactored sqlalchemy tables to pandas dataframe

--- tables.py
+++ tables.py
@@ -1,55 +1,30 @@
 from flask import Flask, Blueprint
-from flask_sqlalchemy import SQLAlchemy
-import datetime
 from flask_restx import Api
-from flask_migrate import Migrate
+import pandas as pd
+import datetime
 
-path1 = 'sqlite:///' + 'databases/DB1.db'
-path2 = 'sqlite:///' + 'databases/DB2.db'
-
-# print(path1) 
-# print(path2)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = path1
-app.config['SQLALCHEMY_BINDS'] = {'DB2' : path2}
-
-db = SQLAlchemy(app)
 
 blueprint = Blueprint('api', __name__, url_prefix='/api')
 api = Api(blueprint, doc='/docs/')
 app.register_blueprint(blueprint)
-# api = Api(app, prefix="/api")
 
-migrate = Migrate(app, db)
+# df1 is table1 and df2 is table2
+df1 = pd.read_csv("databases/table1.csv")
+df2 = pd.read_csv("databases/table2.csv")
 
-class TABLE1(db.Model):
-    __tablename__ = 'table1'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    fname = db.Column(db.String(40), nullable=False)
-    lname = db.Column(db.String(40), nullable=False)
-    real_id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.String(40), nullable=True, default = "ACTIVE")
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now())
-    updated_at = db.Column(db.DateTime, nullable=True, onupdate=datetime.datetime.now())
-    gender = db.Column(db.String(20))
-    customer = db.relationship('TABLE2', backref='table1', uselist=False)
-    def __repr__(self):
-        return f"Cardholder Table: {self.fname} {self.lname}"
+# Function to convert time string to datetime format
+def date_convert(date_to_convert):
+    try:
+        return datetime.datetime.strptime(str(date_to_convert), f'%Y-%m-%d %X.%f')
+    except:
+        return float('nan')
 
+# Converting time string to datetime format for table1
+df1['created_at'] = df1['created_at'].apply(date_convert)
+df1['updated_at'] = df1['updated_at'].apply(date_convert)
 
-
-class TABLE2(db.Model):
-    __bind_key__ = 'DB2'
-    __tablename__ = 'table2'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    fname = db.Column(db.String(40), nullable=False)
-    lname = db.Column(db.String(40), nullable=False)
-    customer_no = db.Column(db.Integer, db.ForeignKey('table1.real_id'))
-    status = db.Column(db.String(40), nullable=True, default = "ACTIVE")
-    offer = db.Column(db.Integer, default=50000)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now())
-    updated_at = db.Column(db.DateTime, nullable=True, onupdate=datetime.datetime.now())
-
-    def __repr__(self):
-        return f"Customer Table: {self.fname} {self.lname}"
+# Converting time string to datetime format for table2
+df2['created_at'] = df2['created_at'].apply(date_convert)
+df2['updated_at'] = df2['updated_at'].apply(date_convert)
