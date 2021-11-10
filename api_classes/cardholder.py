28e9de00377425cd36cd930a2a8d463cc90d74f4
fahid latheef <fahidlatheef@fahid-latheefs-MacBook-Pro.local>
2021-11-11 01:36:17 +0530

    Added pandas version of Cardholder Classes

--- api_classes/cardholder.py
+++ api_classes/cardholder.py
@@ -1,84 +1,78 @@
 import datetime
-from flask_restx import Resource, fields, marshal_with, abort
-from tables import TABLE1
+from flask_restx import Resource, abort
+import pandas as pd
+import json
 
-# TABLE 1 Marshall Structure
 
-resource_fields_table1 = {
-    'id': fields.Integer,
-    'fname': fields.String,
-    'lname': fields.String,
-    'real_id': fields.Integer,
-    'status': fields.String,
-    'created_at': fields.DateTime,
-    'updated_at': fields.DateTime,
-    'gender': fields.String,
-}
+# df is table1
+df = pd.read_csv("databases/table1.csv")
 
-# Cardholder table1 Classes
+# Function to convert time string to datetime format
+def date_convert(date_to_convert):
+    try:
+        return datetime.datetime.strptime(str(date_to_convert), f'%Y-%m-%d %X.%f')
+    except:
+        return float('nan')
+# Converting time string to datetime format
+df['created_at'] = df['created_at'].apply(date_convert)
+df['updated_at'] = df['updated_at'].apply(date_convert)
 
+# Cardholder Classes
 
 class CardHolderFilterByID(Resource):
-    @marshal_with(resource_fields_table1)
     def get(self, cardholder_id):
-        result = TABLE1.query.filter_by(id=cardholder_id).first()
+        result = json.loads(df.loc[df['id']==cardholder_id].to_json(orient='records', date_format='iso'))
         if not result:
             abort(404, message=f"Could not find CardHolder with id: {cardholder_id}")
-        return result
+        return result[0]
 
 
 class CardHolderFilterByRealID(Resource):
-    @marshal_with(resource_fields_table1)
     def get(self, cardholder_real_id):
-        result = TABLE1.query.filter_by(real_id=cardholder_real_id).first()
+        result = json.loads(df.loc[df['real_id']==cardholder_real_id].to_json(orient='records', date_format='iso'))
         if not result:
             abort(404, message=f"Could not find CardHolder with real_id: {cardholder_real_id}")
-        return result
+        return result[0]
 
 
 class CardHolderFilterByDays(Resource):
-    @marshal_with(resource_fields_table1)
     def get(self, days):
-        end = datetime.date.today()
+        end = pd.to_datetime(datetime.date.today())
         start = end - datetime.timedelta(days=days)
-        results = TABLE1.query.filter(TABLE1.created_at <= end).filter(TABLE1.created_at >= start).all()
+        results = json.loads(df.loc[(df['created_at']<=end) & (df['created_at']>=start)].to_json(orient='records', date_format='iso'))
         if not results:
             abort(404, message=f"Could not find CardHolders created in last {days} days.")
         return results
 
 
 class CardHolderFilterLastN(Resource):
-    @marshal_with(resource_fields_table1)
     def get(self, last_n_holder):
-        results = TABLE1.query.filter(TABLE1.id).order_by(TABLE1.id.desc()).limit(last_n_holder).all()
+        results = json.loads(df.tail(last_n_holder).to_json(orient='records', date_format='iso'))
         if not results:
             abort(404, message=f"Could not find any CardHolders")
         return results
 
 
 class CardHolderSearchName(Resource):
-    @marshal_with(resource_fields_table1)
     def get(self, search_name):
-        results = TABLE1.query.filter(TABLE1.fname.like(f'%{search_name}%') | TABLE1.lname.like(f'%{search_name}%')).all()
+        results = json.loads(df.loc[(df['fname'].str.contains(search_name, case=False)) | df['lname'].str.contains(search_name, case=False)].to_json(orient='records', date_format='iso'))
         if not results:
             abort(404, message=f"Could not find any CardHolders that contains '{search_name}'")
         return results
 
 
 class CardHolderCreatedToday(Resource):
-    @marshal_with(resource_fields_table1)
     def get(self):
-        today = datetime.date.today()
-        results = TABLE1.query.filter(TABLE1.created_at >= today).all()
+        today = pd.to_datetime(datetime.date.today())
+        results = json.loads(df.loc[df['created_at']>=today].to_json(orient='records', date_format='iso'))
         if not results:
             abort(404, message=f"Could not find any CardHolders created today")
         return results
 
 
 class CardHolderAll(Resource):
-    @marshal_with(resource_fields_table1)
     def get(self):
-        results = TABLE1.query.order_by(TABLE1.id.desc()).all()
+        results = json.loads(df.to_json(orient='records', date_format='iso'))
         if not results:
             abort(404, message=f"No Data Found")
         return results
