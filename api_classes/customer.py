2c4decde30725f1592a5873abcf985aa5a02c0ed
fahid latheef <fahidlatheef@fahid-latheefs-MacBook-Pro.local>
2021-11-11 01:56:13 +0530

    Changed API classes to pandas

--- api_classes/customer.py
+++ api_classes/customer.py
@@ -1,84 +1,77 @@
 import datetime
-from flask_restx import Resource, fields, marshal_with, abort
-from tables import TABLE2
+from flask_restx import Resource, abort
+import pandas as pd
+import json
 
+# df is table2
+df = pd.read_csv("databases/table2.csv")
 
-# TABLE 2 MARSHALL STRUCTURE
+# Function to convert time string to datetime format
+def date_convert(date_to_convert):
+    try:
+        return datetime.datetime.strptime(str(date_to_convert), f'%Y-%m-%d %X.%f')
+    except:
+        return float('nan')
+# Converting time string to datetime format
+df['created_at'] = df['created_at'].apply(date_convert)
+df['updated_at'] = df['updated_at'].apply(date_convert)
 
-resource_fields_table2 = {
-    'id': fields.Integer,
-    'fname': fields.String,
-    'lname': fields.String,
-    'customer_no': fields.Integer,
-    'status': fields.String,
-    'offer': fields.Integer,
-    'created_at': fields.DateTime,
-    'updated_at': fields.DateTime
-}
-
-# Customer table2 Classes
+# Customer Classes
 
 class CustomerFilterByID(Resource):
-    @marshal_with(resource_fields_table2)
     def get(self, customer_id):
-        result = TABLE2.query.filter_by(id=customer_id).first()
+        result = json.loads(df.loc[df['id']==customer_id].to_json(orient='records', date_format='iso'))
         if not result:
             abort(404, message=f"Could not find Customer with id: {customer_id}")
-        return result
+        return result[0]
 
 
 class CustomerFilterByCustomerNo(Resource):
-    @marshal_with(resource_fields_table2)
     def get(self, customer_customer_no):
-        result = TABLE2.query.filter_by(customer_no=customer_customer_no).first()
+        result = json.loads(df.loc[df['customer_no']==customer_customer_no].to_json(orient='records', date_format='iso'))
         if not result:
             abort(404, message=f"Could not find Customer with customer_no: {customer_customer_no}")
-        return result
+        return result[0]
 
 
 class CustomerFilterByDays(Resource):
-    @marshal_with(resource_fields_table2)
     def get(self, days):
-        end = datetime.date.today()
+        end = pd.to_datetime(datetime.date.today())
         start = end - datetime.timedelta(days=days)
-        results = TABLE2.query.filter(TABLE2.created_at <= end).filter(TABLE2.created_at >= start).all()
+        results = json.loads(df.loc[(df['created_at']<=end) & (df['created_at']>=start)].to_json(orient='records', date_format='iso'))
         if not results:
             abort(404, message=f"Could not find Customers created in last {days} days.")
         return results
 
 
 class CustomerFilterLastN(Resource):
-    @marshal_with(resource_fields_table2)
     def get(self, last_n_customer):
-        results = TABLE2.query.filter(TABLE2.id).order_by(TABLE2.id.desc()).limit(last_n_customer).all()
+        results = json.loads(df.tail(last_n_customer).to_json(orient='records', date_format='iso'))
         if not results:
             abort(404, message=f"Could not find any Customers")
         return results
 
 
 class CustomerSearchName(Resource):
-    @marshal_with(resource_fields_table2)
     def get(self, search_name):
-        results = TABLE2.query.filter(TABLE2.fname.like(f'%{search_name}%') | TABLE2.lname.like(f'%{search_name}%')).all()
+        results = json.loads(df.loc[(df['fname'].str.contains(search_name, case=False)) | df['lname'].str.contains(search_name, case=False)].to_json(orient='records', date_format='iso'))
         if not results:
             abort(404, message=f"Could not find any Customer that contains '{search_name}'")
         return results
 
 
 class CustomerCreatedToday(Resource):
-    @marshal_with(resource_fields_table2)
     def get(self):
-        today = datetime.date.today()
-        results = TABLE2.query.filter(TABLE2.created_at >= today).all()
+        today = pd.to_datetime(datetime.date.today())
+        results = json.loads(df.loc[df['created_at']>=today].to_json(orient='records', date_format='iso'))
         if not results:
             abort(404, message=f"Could not find any Customers created today")
         return results
 
 
 class CustomerAll(Resource):
-    @marshal_with(resource_fields_table2)
     def get(self):
-        results = TABLE2.query.order_by(TABLE2.id.desc()).all()
+        results = json.loads(df.to_json(orient='records', date_format='iso'))
         if not results:
             abort(404, message=f"No Data Found")
         return results
