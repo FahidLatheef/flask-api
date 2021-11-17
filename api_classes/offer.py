288cb2819f855fb73839100b531fc9c633ba8466
Fahid Latheef A <fahid@fplabs.tech>
2021-11-17 19:05:33 +0530

    Fixed Offer API Classes to support Sanic

--- api_classes/offer.py
+++ api_classes/offer.py
@@ -1,8 +1,10 @@
 import datetime
-from flask_restx import Resource, fields, marshal_with, abort
+from sanic.views import HTTPMethodView
+from sanic_restful_api import abort
 import pandas as pd
 import json
 from tables import df1, df2
+from sanic.response import json as sanic_json
 
 # Merging df1 and df2 to df
 df = pd.merge(left=df1, right=df2[['customer_no', 'offer']], left_on='real_id', right_on='customer_no')
@@ -10,70 +12,58 @@ df = pd.merge(left=df1, right=df2[['customer_no', 'offer']], left_on='real_id', 
 # Renaming id to more relevant cardholder_id
 df = df.rename(columns={'id': 'cardholder_id'})
 
-# OFFER MARSHALL STRUCTURE
-
-resource_fields_offer = {
-    'cardholder_id': fields.Integer,
-    'fname': fields.String,
-    'lname': fields.String,
-    'gender': fields.String,
-    'offer': fields.Integer,
-}
-
 # Offer Classes
 
-class OfferFilterByID(Resource):
-    @marshal_with(resource_fields_offer)
-    def get(self, cardholder_id):
-        result = json.loads(df.loc[df['cardholder_id']==cardholder_id].to_json(orient='records', date_format='iso'))
+class OfferFilterByID(HTTPMethodView):
+    async def get(self, request, cardholder_id):
+        filtered_data = df.loc[df['cardholder_id']==cardholder_id]
+        result = json.loads(filtered_data[['cardholder_id', 'fname', 'lname', 'gender', 'offer']].to_json(orient='records', date_format='iso'))
         if not result:
             abort(404, message=f"Could not find Cardholder with id: {cardholder_id}")
-        return result[0]
+        return sanic_json(result)
 
 
-class OfferFilterByDays(Resource):
-    @marshal_with(resource_fields_offer)
-    def get(self, days):
+class OfferFilterByDays(HTTPMethodView):
+    async def get(self, request, days):
         end = pd.to_datetime(datetime.date.today())
         start = end - datetime.timedelta(days=days)
-        results = json.loads(df.loc[(df['created_at']<=end) & (df['created_at']>=start)].to_json(orient='records', date_format='iso'))
+        filtered_data = df.loc[(df['created_at']<=end) & (df['created_at']>=start)]
+        results = json.loads(filtered_data[['cardholder_id', 'fname', 'lname', 'gender', 'offer']].to_json(orient='records', date_format='iso'))
         if not results:
             abort(404, message=f"Could not find CardHolders created in last {days} days.")
-        return results
+        return sanic_json(results)
 
 
-class OfferFilterLastN(Resource):
-    @marshal_with(resource_fields_offer)
-    def get(self, last_n_holder):
-        results = json.loads(df.tail(last_n_holder).to_json(orient='records', date_format='iso'))
+class OfferFilterLastN(HTTPMethodView):
+    async def get(self, request, last_n_holder):
+        filtered_data = df.tail(last_n_holder)
+        results = json.loads(filtered_data[['cardholder_id', 'fname', 'lname', 'gender', 'offer']].to_json(orient='records', date_format='iso'))
         if not results:
             abort(404, message=f"Could not find any CardHolders")
-        return results
+        return sanic_json(results)
 
 
-class OfferSearchName(Resource):
-    @marshal_with(resource_fields_offer)
-    def get(self, search_name):
-        results = json.loads(df.loc[(df['fname'].str.contains(search_name, case=False)) | df['lname'].str.contains(search_name, case=False)].to_json(orient='records', date_format='iso'))
+class OfferSearchName(HTTPMethodView):
+    async def get(self, request, search_name):
+        filtered_data = df.loc[(df['fname'].str.contains(search_name, case=False)) | df['lname'].str.contains(search_name, case=False)]
+        results = json.loads(filtered_data[['cardholder_id', 'fname', 'lname', 'gender', 'offer']].to_json(orient='records', date_format='iso'))
         if not results:
             abort(404, message=f"Could not find any CardHolders that contains '{search_name}'")
-        return results
+        return sanic_json(results)
 
 
-class OfferCreatedToday(Resource):
-    @marshal_with(resource_fields_offer)
-    def get(self):
+class OfferCreatedToday(HTTPMethodView):
+    async def get(self, request):
         today = pd.to_datetime(datetime.date.today())
-        results = json.loads(df.loc[df['created_at']>=today].to_json(orient='records', date_format='iso'))
+        filtered_data = df.loc[df['created_at']>=today]
+        results = json.loads(filtered_data[['cardholder_id', 'fname', 'lname', 'gender', 'offer']].to_json(orient='records', date_format='iso'))
         if not results:
             abort(404, message=f"Could not find any CardHolders created today")
-        return results
+        return sanic_json(results)
 
-
-class OfferAll(Resource):
-    @marshal_with(resource_fields_offer)
-    def get(self):
-        results = json.loads(df.to_json(orient='records', date_format='iso'))
+class OfferAll(HTTPMethodView):
+    async def get(self, request):
+        results = json.loads(df[['cardholder_id', 'fname', 'lname', 'gender', 'offer']].to_json(orient='records', date_format='iso'))
         if not results:
             abort(404, message=f"No Data Found")
-        return results
+        return sanic_json(results)
