3771ad6a7c257d830a30777022566ce5d03ebbbc
Fahid Latheef A <fahid@fplabs.tech>
2021-11-17 19:42:06 +0530

    Fixed all the routes of APIs

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # from flask import render_template, redirect
-from sanic.response import file, redirect, text
-from tables import api, app, jinja
+from sanic.response import redirect
+from tables import app, jinja
 
 from api_classes.cardholder import (
     CardHolderFilterByID,
@@ -31,27 +31,23 @@ from api_classes.offer import (
     OfferAll
 )
 
-# Create tables: Run it once
-# db.create_all()
-# db.session.commit()
-
 # Cardholder Routes
-app.add_route(CardHolderFilterByID, "/cardholder/id/<int:cardholder_id>")
-app.add_route(CardHolderFilterByRealID, "/cardholder/real_id/<int:cardholder_real_id>")
-app.add_route(CardHolderFilterByDays, "/cardholder/created_last_n_days/<int:days>")
-app.add_route(CardHolderFilterLastN, "/cardholder/last_n_person/<int:last_n_holder>")
-app.add_route(CardHolderSearchName, "/cardholder/search/<string:search_name>")
-app.add_route(CardHolderCreatedToday, "/cardholder/created_today")
-app.add_route(CardHolderAll, "/cardholder/full_data")
+app.add_route(CardHolderFilterByID.as_view(), "/cardholder/id/<cardholder_id:int>", methods=['GET'])
+app.add_route(CardHolderFilterByRealID.as_view(), "/cardholder/real_id/<cardholder_real_id:int>", methods=['GET'])
+app.add_route(CardHolderFilterByDays.as_view(), "/cardholder/created_last_n_days/<days:int>", methods=['GET'])
+app.add_route(CardHolderFilterLastN.as_view(), "/cardholder/last_n_person/<last_n_holder:int>", methods=['GET'])
+app.add_route(CardHolderSearchName.as_view(), "/cardholder/search/<search_name:str>", methods=['GET'])
+app.add_route(CardHolderCreatedToday.as_view(), "/cardholder/created_today", methods=['GET'])
+app.add_route(CardHolderAll.as_view(), "/cardholder/full_data", methods=['GET'])
 
 # Customer Routes
-app.add_route(CustomerFilterByID.as_view(), "/customer/id/<int:customer_id>")
-app.add_route(CustomerFilterByCustomerNo, "/customer/customer_no/<int:customer_customer_no>")
-app.add_route(CustomerFilterByDays, "/customer/created_last_n_days/<int:days>")
-app.add_route(CustomerFilterLastN, "/customer/last_n_person/<int:last_n_customer>")
-app.add_route(CustomerSearchName, "/customer/search/<string:search_name>")
-app.add_route(CustomerCreatedToday, "/customer/created_today")
-app.add_route(CustomerAll.as_view(), "/customer/full_data")
+app.add_route(CustomerFilterByID.as_view(), "/customer/id/<customer_id:int>", methods=['GET'])
+app.add_route(CustomerFilterByCustomerNo.as_view(), "/customer/customer_no/<customer_customer_no:int>", methods=['GET'])
+app.add_route(CustomerFilterByDays.as_view(), "/customer/created_last_n_days/<days:int>", methods=['GET'])
+app.add_route(CustomerFilterLastN.as_view(), "/customer/last_n_person/<last_n_customer:int>", methods=['GET'])
+app.add_route(CustomerSearchName.as_view(), "/customer/search/<search_name:str>", methods=['GET'])
+app.add_route(CustomerCreatedToday.as_view(), "/customer/created_today", methods=['GET'])
+app.add_route(CustomerAll.as_view(), "/customer/full_data", methods=['GET'])
 
 # Offer Routes
 app.add_route(OfferFilterByID.as_view(), "/offer/id/<cardholder_id:int>", methods=['GET'])
