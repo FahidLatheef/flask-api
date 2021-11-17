a232f7ff2f0e9d988d2202af66feef9b2f7584d8
Fahid Latheef A <fahid@fplabs.tech>
2021-11-17 19:04:44 +0530

    Fixed Offer routes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import render_template, redirect
-from tables import api, app
+# from flask import render_template, redirect
+from sanic.response import file, redirect, text
+from tables import api, app, jinja
 
 from api_classes.cardholder import (
     CardHolderFilterByID,
@@ -35,38 +36,38 @@ from api_classes.offer import (
 # db.session.commit()
 
 # Cardholder Routes
-api.add_resource(CardHolderFilterByID, "/cardholder/id/<int:cardholder_id>")
-api.add_resource(CardHolderFilterByRealID, "/cardholder/real_id/<int:cardholder_real_id>")
-api.add_resource(CardHolderFilterByDays, "/cardholder/created_last_n_days/<int:days>")
-api.add_resource(CardHolderFilterLastN, "/cardholder/last_n_person/<int:last_n_holder>")
-api.add_resource(CardHolderSearchName, "/cardholder/search/<string:search_name>")
-api.add_resource(CardHolderCreatedToday, "/cardholder/created_today")
-api.add_resource(CardHolderAll, "/cardholder/full_data")
+app.add_route(CardHolderFilterByID, "/cardholder/id/<int:cardholder_id>")
+app.add_route(CardHolderFilterByRealID, "/cardholder/real_id/<int:cardholder_real_id>")
+app.add_route(CardHolderFilterByDays, "/cardholder/created_last_n_days/<int:days>")
+app.add_route(CardHolderFilterLastN, "/cardholder/last_n_person/<int:last_n_holder>")
+app.add_route(CardHolderSearchName, "/cardholder/search/<string:search_name>")
+app.add_route(CardHolderCreatedToday, "/cardholder/created_today")
+app.add_route(CardHolderAll, "/cardholder/full_data")
 
 # Customer Routes
-api.add_resource(CustomerFilterByID, "/customer/id/<int:customer_id>")
-api.add_resource(CustomerFilterByCustomerNo, "/customer/customer_no/<int:customer_customer_no>")
-api.add_resource(CustomerFilterByDays, "/customer/created_last_n_days/<int:days>")
-api.add_resource(CustomerFilterLastN, "/customer/last_n_person/<int:last_n_customer>")
-api.add_resource(CustomerSearchName, "/customer/search/<string:search_name>")
-api.add_resource(CustomerCreatedToday, "/customer/created_today")
-api.add_resource(CustomerAll, "/customer/full_data")
+app.add_route(CustomerFilterByID.as_view(), "/customer/id/<int:customer_id>")
+app.add_route(CustomerFilterByCustomerNo, "/customer/customer_no/<int:customer_customer_no>")
+app.add_route(CustomerFilterByDays, "/customer/created_last_n_days/<int:days>")
+app.add_route(CustomerFilterLastN, "/customer/last_n_person/<int:last_n_customer>")
+app.add_route(CustomerSearchName, "/customer/search/<string:search_name>")
+app.add_route(CustomerCreatedToday, "/customer/created_today")
+app.add_route(CustomerAll.as_view(), "/customer/full_data")
 
 # Offer Routes
-api.add_resource(OfferFilterByID, "/offer/id/<int:cardholder_id>")
-api.add_resource(OfferFilterByDays, "/offer/created_last_n_days/<int:days>")
-api.add_resource(OfferFilterLastN, "/offer/last_n_person/<int:last_n_holder>")
-api.add_resource(OfferSearchName, "/offer/search/<string:search_name>")
-api.add_resource(OfferCreatedToday, "/offer/created_today")
-api.add_resource(OfferAll, "/offer/full_data")
+app.add_route(OfferFilterByID.as_view(), "/offer/id/<cardholder_id:int>", methods=['GET'])
+app.add_route(OfferFilterByDays.as_view(), "/offer/created_last_n_days/<days:int>", methods=['GET'])
+app.add_route(OfferFilterLastN.as_view(), "/offer/last_n_person/<last_n_holder:int>", methods=['GET'])
+app.add_route(OfferSearchName.as_view(), "/offer/search/<search_name:str>", methods=['GET'])
+app.add_route(OfferCreatedToday.as_view(), "/offer/created_today", methods=['GET'])
+app.add_route(OfferAll.as_view(), "/offer/full_data", methods=['GET'])
 
 @app.route('/')
-def index():
-    return render_template('home.html', title="Home")
+async def index(request):
+    return jinja.render('home.html', request=request, title="Sanic API Home ")
 
 @app.route('/home')
-def home():
+async def home(request):
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True, port=8000)
